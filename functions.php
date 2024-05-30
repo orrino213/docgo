4bcd1667649f5469cf8ede9478534263126ce7a1
ArneZarg <zargarian.arne@gmail.com>
2024-05-29 21:08:13 -0700

    skeleton

--- functions.php
+++ functions.php
@@ -1,12 +1,48 @@
+<!-- <link rel="apple-touch-icon" href="images/favicon.png">
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="font/flaticon.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/venobox.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css">
+
+    <link rel="stylesheet" href="css/style.css"> -->
+
 <?php
+
+/*
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.scrollUp.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/waypoints.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/venobox.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCe2q1gFSd75jKUnTDPq0nZWaMlEu6vL30"></script>
+<script src="js/google-map.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+
+*/
 
 // Enqueue styles and scripts
 function your_theme_enqueue_scripts() {
-    // Enqueue main stylesheet
+    // Enqueue main stylesheets
+    wp_enqueue_style('template-style-1', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/css/bootstrap.min.css');
+    wp_enqueue_style('template-style-3', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/css/flaticon.css');
+    wp_enqueue_style('template-style-4', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/css/owl.carousel.min.css');
+    wp_enqueue_style('template-style-7', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/css/style.css');
     wp_enqueue_style( 'your-theme-style', get_stylesheet_uri() .'/style.css' );
 
-    // // Enqueue a custom script (example)
-    // wp_enqueue_script( 'your-theme-script', get_template_directory_uri() . '/js/custom.js', array('jquery'), null, true );
+    // // Enqueue a custom scripts
+    wp_enqueue_script( 'your-theme-script-1', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/js/jquery-3.6.0.min.js');
+    wp_enqueue_script( 'your-theme-script-2', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/js/bootstrap.bundle.min.js');
+    wp_enqueue_script( 'your-theme-script-3', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/js/jquery.scrollUp.min.js');
+    wp_enqueue_script( 'your-theme-script-4', get_template_directory_uri() . '/consulpro html template/consulpro-v2.3/js/owl.carousel.min.js');
 }
 add_action( 'wp_enqueue_scripts', 'your_theme_enqueue_scripts' );
 
--- header.php
+++ header.php
@@ -7,37 +7,10 @@
     <title>Consulpro HTML5 Consultation Template</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" href="images/favicon.png">
-    <link rel="shortcut icon" href="images/favicon.ico">
-
-    <!-- Css Library -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="font/flaticon.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/venobox.min.css">
-    <link rel="stylesheet" href="css/jquery-ui.min.css">
-
-    <!-- Style css -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <?php wp_head(); ?>
 </head>
 
 <body>
-
-    <!-- Preloader start-->
-    <div id="preloader">
-        <div class="preloader">
-            <span></span>
-            <span></span>
-        </div>
-    </div>
-    <!-- Preloader end  -->
 
     <!-- Index Header Start -->
     <header class="header index-header">

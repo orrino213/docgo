e72d4f6c06b4f365802e657b3e5373d22cce4117
ArneZarg <zargarian.arne@gmail.com>
2024-06-10 20:33:28 -0700

    styles for blog-section

--- category-care-for-all.php
+++ category-care-for-all.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
-
+<style>
+	.main-body{
+		padding-top:100px;
+	}
+</style>
+<div class="main-body">
 <!-- Blog Section -->
 <?php include get_template_directory() . '/snippets/blog-section.php' ?>
-
+</div>
 <?php get_footer(); ?>
